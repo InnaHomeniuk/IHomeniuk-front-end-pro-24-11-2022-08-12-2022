6d5740da5736785b0d0128861980d252bec90537
InnaHomeniuk <pivanova777@gmail.com>
2022-12-22 22:59:16 +0200

    added variebles min/max, change neg/pos

--- homework-9/arrayCycle.js
+++ homework-9/arrayCycle.js
@@ -1,31 +1,34 @@
 console.group('Домашнє завдання №9');
-const arrayNumbers = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+const arrayNumbers = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47]
 let sum = 0;
 let sumPositive = 0;
 for (let i = 0; i < arrayNumbers.length; i++) {
-    sum = sum + arrayNumbers[i];
     if (arrayNumbers[i] > 0) {
         sumPositive = sumPositive + 1;
+        sum = sum + arrayNumbers[i];
     }
 }
 console.log(`Сумма елементів: ${sum}, кількість позитивних елементів: ${sumPositive}`);
 
+
+let min = arrayNumbers[0];
 let index = 0;
 for (let i = 0; i < arrayNumbers.length; i++) {
-    if (arrayNumbers[i] < arrayNumbers[0]) {
-        arrayNumbers[0] = arrayNumbers[i];
+    if (arrayNumbers[i] < min) {
+        min = arrayNumbers[i];
         index = i;
     }
 }
-console.log(`Мінімальне значення: ${arrayNumbers[0]}, його порядковий номер: ${index}`);
+console.log(`Мінімальне значення: ${min}, його порядковий номер: ${index}`);
 
+let max = arrayNumbers[0];
 for (let i = 0; i < arrayNumbers.length; i++) {
-    if (arrayNumbers[i] > arrayNumbers[0]) {
-        arrayNumbers[0] = arrayNumbers[i];
+    if (arrayNumbers[i] > max) {
+        max = arrayNumbers[i];
         index = i;
     }
 }
-console.log(`Максимальне значення: ${arrayNumbers[0]}, його порядковий номер: ${index}`);
+console.log(`Максимальне значення: ${max}, його порядковий номер: ${index}`);
 
 let sumNegative = 0;
 for (let i = 0; i < arrayNumbers.length; i++) {
@@ -35,12 +38,12 @@ for (let i = 0; i < arrayNumbers.length; i++) {
 }
 console.log(`Кількість негативних елементів: ${sumNegative}`);
 
-multNegative = 1;
+let multPositive = 1;
 for (let i = 0; i < arrayNumbers.length; i++) {
-    if (arrayNumbers[i] < 0) {
-        multNegative = multNegative * arrayNumbers[i];
+    if (arrayNumbers[i] > 0) {
+        multPositive = multPositive * arrayNumbers[i];
     }
 }
-console.log(`Добуток негативних елементів: ${multNegative}`);
+console.log(`Добуток позитивних елементів: ${multPositive}`);
 
 console.groupEnd();
