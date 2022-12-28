08be795e78ecf53e4a3741470d4d55150ce2bcdf
InnaHomeniuk <pivanova777@gmail.com>
2022-12-28 10:21:51 +0200

    for change if

--- homework-12/deleteFunction.js
+++ homework-12/deleteFunction.js
@@ -1,11 +1,7 @@
-function removeElement(array, element) {
-    let index = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array.includes(element)) {
-            index = array.indexOf(element);
-            array.splice(index, 1);
-        }
-    }
-    return array;
-}
+function removeElement(array, element) { 
+  if (array.includes(element)) { 
+        array.splice(array.indexOf(element), 1); 
+      } 
+  return array; 
+} 
 console.log(removeElement([1, 2, 3, 4, 5, 6, 7], 5));
