b0740b3e642187ea053b890db30957286ee2e829
InnaHomeniuk <pivanova777@gmail.com>
2023-01-23 20:00:49 +0200

    code improvement

--- homework-21/createButton.js
+++ homework-21/createButton.js
@@ -1,32 +1,12 @@
-function addedUrl() {
-  const link = prompt("уведіть посилання")
-  //console.log(link);
-  linkArr = link.split("");
-  let arrText = [];
-  const protocolHttp = ['h', 't', 't', 'p'];
-  const protocolHttps = ['h', 't', 't', 'p', 's'];
-  for (let i = 0; i < 4; i++) {
-    arrText.push(linkArr[i]);
-  }
-  console.log(arrText);
-  for (let i = 0; i < arrText.length; i++) {
-    if (arrText[i] === protocolHttp[i] || arrText[i] === protocolHttps[i]) {
-      continue
+function callLink() {
+  const link = prompt("уведіть посилання");
+  function addedUrl() {
+    if (link.includes('http') || link.includes('https')) {
+      return window.location.href = link;
     } else {
-      linkArr.unshift('h', 't', 't', 'p', 's', ':', '/', '/');
-      //console.log(linkArr)
-      break
+      return window.location.href = `https://${link}`
     }
   }
-  stringLink = linkArr.join("");
-  console.log(stringLink);
-
-  function addedUrl2() {
-    window.location.href = stringLink;
-  };
-  document.getElementById('href_button').addEventListener('click', addedUrl2);
-};
-document.getElementById('input_button').addEventListener('click', addedUrl);
-
-
-
+  document.getElementById('href_button').addEventListener('click', addedUrl);
+}
+document.getElementById('input_button').addEventListener('click', callLink);
